df8f5e16457303af342ddba2698d2bd3f884489f
Curtis <curtis@cstudio.ca>
2022-09-04 18:48:46 -0700

    api data

--- src/components/Data.js
+++ src/components/Data.js
@@ -4,10 +4,12 @@ import { API_KEY } from '../api/config';
 
 const Data = () => {
 	const [stockData, setStockData] = useState({});
+	const [stockQuote, setStockQuote] = useState({});
 	const [symbol, setSymbol] = useState(0); // Symbol
 	const [name, setName] = useState(''); // Name
 	const [sector, setSector] = useState(''); // Sector
 	const [industry, setIndustry] = useState(''); // Industry
+	const [price, setPrice] = useState(0); // ['Global Quote']['05. price']
 	const [currency, setCurrency] = useState(''); // Currency
 	const [marketCap, setMarketCap] = useState(0); // MarketCapitalization
 	const [sharesOutstanding, setSharesOutstanding] = useState(0); // SharesOutstanding
@@ -20,23 +22,24 @@ const Data = () => {
 	// const [movingAvg50, setMovingAvg50] = useState(0); // 50DayMovingAverage
 	// const [movingAvg200, setMovingAvg200] = useState(0); // 200DayMovingAverage
 
-	const SYMBOL = 'AMD';
-	const URL = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${SYMBOL}&apikey=${API_KEY}`;
+	const SYMBOL = 'F';
+	const URL_DATA = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${SYMBOL}&apikey=${API_KEY}`;
+	const URL_QUOTE = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${SYMBOL}&apikey=${API_KEY}`;
 
 	useEffect(() => {
 		fetchStockData();
-	}, []);
+	}, [SYMBOL]);
 
 	const fetchStockData = async () => {
-		const response = await Axios(URL);
+		const response = await Axios(URL_DATA);
 		setStockData(response.data);
 		setSymbol(stockData.Symbol);
 		setName(stockData.Name);
 		setSector(stockData.Sector);
 		setIndustry(stockData.Industry);
-		setCurrency(stockData.Currency);
 		setMarketCap(stockData.MarketCapitalization);
 		setSharesOutstanding(stockData.SharesOutstanding);
+		setCurrency(stockData.Currency);
 		setDivYield(stockData.DividendYield);
 		setPERatio(stockData.PERatio);
 		setForwardPE(stockData.ForwardPE);
@@ -47,6 +50,16 @@ const Data = () => {
 		// setMovingAvg200(stockData.200DayMovingAverage);
 	};
 
+	useEffect(() => {
+		fetchStockQuote();
+	}, [SYMBOL]);
+
+	const fetchStockQuote = async () => {
+		const response = await Axios(URL_QUOTE);
+		setStockQuote(response.data);
+		setPrice(stockQuote['Global Quote']['05. price']);
+		console.log(price);
+	};
 	return (
 		<>
 			<div className="data">
@@ -55,17 +68,14 @@ const Data = () => {
 				<div>Sector: {sector}</div>
 				<div>Industry: {industry}</div>
 				<div>
-					Price: {marketCap / sharesOutstanding} {currency}
+					Price: $ {price} {currency}
 				</div>
-				<div>Market Cap:{marketCap}</div>
-				<div>Dividend Yield: {divYield}</div>
+				<div>Dividend Yield: {divYield * 100} %</div>
 				<div>PE Ratio: {PERatio}</div>
 				<div>Forward PE: {ForwardPE}</div>
 				<div>EPS: {EPS}</div>
-				<div>PEG Ratio{PEGRatio}</div>
+				<div>PEG Ratio: {PEGRatio}</div>
 				<div>Price to Sales: {priceToSalesRatio}</div>
-				<div>50 Day Moving Avg: {priceToSalesRatio}</div>
-				<div>200 Day Moving Avg: {priceToSalesRatio}</div>
 			</div>
 		</>
 	);
