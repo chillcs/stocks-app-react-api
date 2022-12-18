69352adc2632514c328741a6f270260226c7fe18
Curtis <curtis@cstudio.ca>
2022-12-18 14:55:45 -0800

    refactor

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,22 +2,18 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 
 const Main = () => {
+	const [isLoading, setIsLoading] = useState(true);
 	const [stocksUSD, setStocksUSD] = useState([]);
 	const [updateStocksUSD, setUpdateStocksUSD] = useState(false);
-	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
-		fetch(
-			'https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/CAT,COST,EXPI,GOOG,HD',
-			{
-				method: 'GET',
-				headers: {
-					'X-RapidAPI-Key':
-						'69aafb449cmsh69b03d490febf69p1748cdjsnc60a61c4e79b',
-					'X-RapidAPI-Host': 'yahoo-finance15.p.rapidapi.com',
-				},
-			}
-		)
+		fetch('https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/CAT,COST,EXPI,GOOG,HD', {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': '69aafb449cmsh69b03d490febf69p1748cdjsnc60a61c4e79b',
+				'X-RapidAPI-Host': 'yahoo-finance15.p.rapidapi.com',
+			},
+		})
 			.then((data) => data.json())
 			.then((data) => {
 				setStocksUSD(data);
@@ -77,10 +73,7 @@ const Main = () => {
 				<div className="portfolio">
 					<div className="portfolio-title">Stocks (USD)</div>
 					<div className="portfolio-btns">
-						<button
-							className="portfolio-btn"
-							onClick={() => setUpdateStocksUSD(true)}
-						>
+						<button className="portfolio-btn" onClick={() => setUpdateStocksUSD(true)}>
 							Update
 						</button>
 					</div>
@@ -88,17 +81,17 @@ const Main = () => {
 				<div className="table">
 					<div className="row">
 						<div className="column column-heading">Symbol</div>
-						<div className="column column-heading">Price</div>
 						<div className="column column-heading">Quantity</div>
+						<div className="column column-heading">Price</div>
 						<div className="column column-heading">Value</div>
 					</div>
 					{HOLDINGS_USD.map((holding, i) => {
 						return (
 							<div className="row" key={i}>
 								<div className="column">{holding.symbol}</div>
-								<div className="column">{stocksUSD[i]['ask']}</div>
 								<div className="column">{holding.quantity}</div>
-								<div className="column">xx</div>
+								<div className="column">${stocksUSD[i]['ask'].toFixed(2).toLocaleString()}</div>
+								<div className="column">${(stocksUSD[i]['ask'] * holding.quantity).toFixed(2).toLocaleString()}</div>
 							</div>
 						);
 					})}
